281af2a4cbe5882a7717a3fac5da45f07df6d1cc
Intybyte <vaan.testwork@gmail.com>
2025-07-23 23:00:51 +0200

    Cleanup

--- src/main/java/translate/UmlTranslator.java
+++ src/main/java/translate/UmlTranslator.java
@@ -5,28 +5,22 @@ import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.Problem;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.ConstructorDeclaration;
-import com.github.javaparser.ast.body.EnumConstantDeclaration;
 import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.RecordDeclaration;
 import com.github.javaparser.ast.nodeTypes.NodeWithMembers;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.visitor.VoidVisitor;
+import translate.component.ClassWriter;
 import translate.component.EnumWriter;
+import translate.component.InterfaceWriter;
 import translate.component.MemberFormatter;
 import translate.component.RecordWriter;
 import translate.component.SetTranslatingComponent;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -35,24 +29,21 @@ import java.util.Set;
 public class UmlTranslator implements Translator {
 
     private final Set<SetTranslatingComponent<?>> componentTranslators = new HashSet<>();
-    private final Set<ClassOrInterfaceDeclaration> classSet;
-    private final Set<ClassOrInterfaceDeclaration> interfaceSet;
     private Boolean error = false;
 
     public static ClassDiagramConfig config = new ClassDiagramConfig.DefaultDirector().construct();
 
 
     public UmlTranslator() {
-        classSet = new HashSet<>();
-        interfaceSet = new HashSet<>();
-
+        componentTranslators.add(new ClassWriter());
+        componentTranslators.add(new InterfaceWriter());
         componentTranslators.add(new EnumWriter());
         componentTranslators.add(new RecordWriter());
     }
 
     @Override
     public void addClass(ClassOrInterfaceDeclaration c) {
-        if (!c.isInterface()) classSet.add(c);
+        addNode(c);
     }
 
     @Override
@@ -62,7 +53,7 @@ public class UmlTranslator implements Translator {
 
     @Override
     public void addInterface(ClassOrInterfaceDeclaration i) {
-        if (i.isInterface()) interfaceSet.add(i);
+        addNode(i);
     }
 
     @Override
@@ -122,9 +113,7 @@ public class UmlTranslator implements Translator {
 
         if (!config.isShowColoredAccessSpecifiers()) sb.append("skinparam classAttributeIconSize 0\n");
 
-        writeClasses(sb); // TODO: it doesn't understand inner classes
         writeAssociations(sb);
-        writeInterfaces(sb);
 
         for (var writer : componentTranslators) {
             Map<String, List<String>> map = writer.write();
@@ -154,14 +143,7 @@ public class UmlTranslator implements Translator {
     }
 
     private void writeAssociations(StringBuilder sb) {
-
         HashSet<String> temp = new HashSet<>();
-        for (ClassOrInterfaceDeclaration c : classSet) {
-            temp.add(MemberFormatter.fullSimpleName(c));
-        }
-        for (ClassOrInterfaceDeclaration c : interfaceSet) {
-            temp.add(MemberFormatter.fullSimpleName(c));
-        }
 
         for (SetTranslatingComponent<?> entry : componentTranslators) {
             for (Node r : entry.getSet()) {
@@ -169,252 +151,37 @@ public class UmlTranslator implements Translator {
             }
         }
 
-        for (ClassOrInterfaceDeclaration c : classSet) {
+        for (SetTranslatingComponent<?> entry : componentTranslators) {
+            if (!entry.type().isAssignableFrom(NodeWithMembers.class)) {
+                continue;
+            }
 
-            for (FieldDeclaration f : c.getFields()) {
+            for (Node node : entry.getSet()) {
 
-                if (temp.contains(f.getVariables().get(0).getType().asString())) {
+                if (!(node instanceof NodeWithMembers<?> c)) {
+                    break;
+                }
 
-                    sb.append(MemberFormatter.fullSimpleName(c));
+                for (FieldDeclaration f : c.getFields()) {
+
+                    if (!temp.contains(f.getVariables().get(0).getType().asString())) {
+                        continue;
+                    }
+
+                    sb.append(MemberFormatter.fullSimpleName(node));
                     sb.append("--");
-//                    sb.append("\"-");
+                    //                    sb.append("\"-");
                     sb.append("\"");
-                    writeModifiers(f.getModifiers(), sb);
+                    sb.append(MemberFormatter.modifiers(f.getModifiers()));
                     sb.append(f.getVariables().get(0).getName());
                     sb.append("\" ");
                     sb.append(f.getVariables().get(0).getType().asString());
                     sb.append("\n");
+
                 }
 
             }
-
         }
 
     }
-
-
-    private void writeClasses(StringBuilder sb) {
-
-        for (ClassOrInterfaceDeclaration c : classSet) {
-            writeClass(c, sb);
-        }
-
-    }
-
-    private void writeClass(ClassOrInterfaceDeclaration c, StringBuilder sb) {
-
-        sb.append(c.isAbstract() ? "abstract " : "class ");
-        sb.append(MemberFormatter.fullSimpleName(c));
-        sb.append("{");
-        sb.append("\n");
-
-//        for(ClassOrInterfaceDeclaration c1:c.get)
-
-        //attributes
-        if (config.isShowAttributes()) {
-            writeAttributes(c, sb);
-        }
-
-        if (config.isShowMethods()) {
-            //methods
-            writeConstructors(c, sb);
-            writeMethods(c, sb);
-        }
-
-        sb.append("}\n");
-
-        //implemented interfaces
-        sb.append(MemberFormatter.nodeWithImplements(c));
-
-        //extended classes
-        sb.append(MemberFormatter.nodeWithExtends(c));
-    }
-
-    private void writeAttributes(NodeWithMembers<?> c, StringBuilder sb) {
-
-        for (FieldDeclaration f : c.getFields()) {
-
-            writeField(f, sb);
-        }
-
-    }
-
-    private void writeField(FieldDeclaration f, StringBuilder sb) {
-
-        writeModifiers(f.getModifiers(), sb);
-        sb.append(f.getVariables().get(0).getName());
-        sb.append(" : ");
-        sb.append(f.getVariables().get(0).getType().asString());
-        sb.append("\n");
-
-    }
-
-
-    private void writeConstructors(NodeWithMembers<?> c, StringBuilder sb) {
-
-        for (ConstructorDeclaration m : c.getConstructors()) {
-            writeConstructor(m, sb);
-        }
-
-    }
-
-    private void writeConstructor(ConstructorDeclaration m, StringBuilder sb) {
-
-        writeModifiers(m.getModifiers(), sb);
-        sb.append(m.getName());
-        sb.append("(");
-
-        for (Parameter p : m.getParameters()) {
-
-            sb.append(p.getName());
-            sb.append(" : ");
-            sb.append(p.getType().asString());
-            sb.append(", ");
-
-        }
-        if (m.getParameters().size() > 0) {
-            sb.deleteCharAt(sb.length() - 1);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        sb.append(")");
-        sb.append("\n");
-        //sb.append(" : ");
-        //sb.append(m.getType().asString());
-    }
-
-
-    private void writeMethods(NodeWithMembers<?> c, StringBuilder sb) {
-
-        for (MethodDeclaration m : c.getMethods()) {
-
-            writeMethod(m, sb);
-            sb.append("\n");
-
-        }
-
-    }
-
-    private void writeMethod(MethodDeclaration m, StringBuilder sb) {
-
-        writeModifiers(m.getModifiers(), sb);
-        sb.append(m.getName());
-        sb.append("(");
-
-        for (Parameter p : m.getParameters()) {
-
-            sb.append(p.getName());
-            sb.append(" : ");
-            sb.append(p.getType().asString());
-            sb.append(", ");
-
-        }
-        if (m.getParameters().size() > 0) {
-            sb.deleteCharAt(sb.length() - 1);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        sb.append(")");
-        sb.append(" : ");
-
-        sb.append(m.getType().asString());
-
-
-    }
-
-    private void writeModifiers(NodeList<Modifier> modifiers, StringBuilder sb) {
-
-        for (Modifier mod : modifiers) {
-            switch (mod.getKeyword()) {
-
-
-                case STATIC:
-                    sb.append("{static} ");
-                    break;
-
-                case ABSTRACT:
-                    sb.append("{abstract} ");
-                    break;
-
-                case PUBLIC:
-                    sb.append("+ ");
-                    break;
-
-                case PRIVATE:
-                    sb.append("- ");
-                    break;
-
-                case PROTECTED:
-                    sb.append("# ");
-                    break;
-
-                //TODO:package visibility not shown yet
-                case DEFAULT:
-                    sb.append("~ ");
-                    break;
-
-            }
-        }
-
-    }
-
-//    private void writeInterfaces(StringBuilder sb){
-//
-//        for(ClassOrInterfaceDeclaration i: interfaceSet){
-//            sb.append("interface ");
-//            sb.append(i.getName());
-//            sb.append("\n");
-//
-//            for(ClassOrInterfaceType e: i.getExtendedTypes()){
-//
-//                sb.append(i.getName());
-//                sb.append(" --|> ");
-//                sb.append(e.getName());
-//                sb.append("\n");
-//            }
-//
-//        }
-//
-//    }
-
-    private void writeInterfaces(StringBuilder sb) {
-
-        for (ClassOrInterfaceDeclaration c : interfaceSet) {
-            writeInterface(c, sb);
-        }
-
-    }
-
-    private void writeInterface(ClassOrInterfaceDeclaration c, StringBuilder sb) {
-
-        sb.append("interface ");
-        sb.append(MemberFormatter.fullSimpleName(c));
-        sb.append("{");
-        sb.append("\n");
-
-//        for(ClassOrInterfaceDeclaration c1:c.get)
-
-        //attributes
-        if (config.isShowAttributes()) {
-            writeAttributes(c, sb);
-        }
-
-        if (config.isShowMethods()) {
-            //methods
-            writeConstructors(c, sb);
-            writeMethods(c, sb);
-        }
-
-        sb.append("}\n");
-
-        for (ClassOrInterfaceType e : c.getExtendedTypes()) {
-
-            sb.append(MemberFormatter.fullSimpleName(c));
-            sb.append(" --|> ");
-            sb.append(MemberFormatter.fullSimpleName(e));
-            sb.append("\n");
-        }
-
-
-    }
-
-
 }
