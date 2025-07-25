12eb13d81cdd065e9a06606aff8d7befaed8e0db
Intybyte <vaan.testwork@gmail.com>
2025-07-25 11:24:12 +0200

    Add base for method map

--- src/main/java/translate/translator/ComplexityTranslator.java
+++ src/main/java/translate/translator/ComplexityTranslator.java
@@ -1,8 +1,11 @@
 package translate.translator;
 
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.nodeTypes.NodeWithMembers;
 import translate.ClassDiagramConfig;
 import translate.complexity.ComplexityEvaluator;
+import translate.complexity.ComplexityMetricResult;
 import translate.complexity.clazz.DITEvaluator;
 import translate.component.MemberFormatter;
 
@@ -12,17 +15,33 @@ import java.util.Map;
 
 public class ComplexityTranslator implements Translator {
     public static ClassDiagramConfig config = new ClassDiagramConfig.DefaultDirector().construct();
-
     private Boolean error = false;
-    private final Map<String, Node> map = new HashMap<>();
-    private final ComplexityEvaluator.Clazz[] evaluators = {
+
+    private final Map<String, Node> classMap = new HashMap<>();
+    private final Map<String, Map<String, MethodDeclaration>> methodMap = new HashMap<>();
+
+    private static final ComplexityEvaluator.Clazz[] evaluators = {
             new DITEvaluator()
     };
 
     @Override
     public void addNode(Node node) {
         String fullName = MemberFormatter.fullPackageName(node);
-        map.put(fullName, node);
+        classMap.put(fullName, node);
+
+        //TODO: this doesn't include inherited methods
+        if (node instanceof NodeWithMembers<?> nwm) {
+            Map<String, MethodDeclaration> methods = methodMap.get(fullName);
+            if (methods == null) {
+                methods = new HashMap<>();
+            }
+
+            for (MethodDeclaration method : nwm.getMethods()) {
+                methods.put(method.getNameAsString(), method);
+            }
+
+            methodMap.put(fullName, methods);
+        }
     }
 
     @Override
@@ -30,12 +49,25 @@ public class ComplexityTranslator implements Translator {
         error = b;
     }
 
+    public ComplexityMetricResult[] evaluateClass(String fullClassName) {
+        ComplexityMetricResult[] result = new ComplexityMetricResult[evaluators.length];
+        for (int i = 0; i < evaluators.length; i++) {
+            result[i] = evaluators[i].calculate(classMap.get(fullClassName));
+        }
+
+        return result;
+    }
+
     @Override
     public ClassDiagramConfig getConfig() {
         return config;
     }
 
-    public JList<String> toComplexityList() {
-        return new JList<>(map.keySet().toArray(String[]::new));
+    public JList<String> getClassJList() {
+        return new JList<>(classMap.keySet().toArray(String[]::new));
+    }
+
+    public JList<String> getMethodsJList(String className) {
+        return new JList<>(methodMap.get(className).keySet().toArray(String[]::new));
     }
 }
