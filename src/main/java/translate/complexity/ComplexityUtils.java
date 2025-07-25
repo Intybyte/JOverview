08dedff489bcc55923e66666f3f54dbaa8ef089f
Intybyte <vaan.testwork@gmail.com>
2025-07-25 11:23:22 +0200

    Make DITEvaluator work + base for resolution

---
+++ src/main/java/translate/complexity/ComplexityUtils.java
@@ -0,0 +1,73 @@
+package translate.complexity;
+
+
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.resolution.SymbolResolver;
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
+import java.io.File;
+import java.util.List;
+import java.util.Stack;
+
+public class ComplexityUtils {
+    private static SymbolResolver resolver;
+
+    public static void initialize(File projectDirectory) {
+        CombinedTypeSolver typeSolver = new CombinedTypeSolver();
+        typeSolver.add(new ReflectionTypeSolver());
+
+        addJavaParserTypeSolvers(projectDirectory, typeSolver);
+
+
+        ParserConfiguration config = new ParserConfiguration();
+
+        resolver = new JavaSymbolSolver(typeSolver);
+
+        config.setSymbolResolver(resolver);
+        StaticJavaParser.setConfiguration(config);
+    }
+
+    private static void addJavaParserTypeSolvers(File projectDirectory, CombinedTypeSolver typeSolver) {
+        File[] files = projectDirectory.listFiles();
+        if (files == null) {
+            typeSolver.add(
+                    new JavaParserTypeSolver(
+                            sourceOf(projectDirectory)
+                    )
+            );
+            return;
+        }
+
+        Stack<File> fileList = new Stack<>();
+        fileList.addAll(List.of(files));
+
+        while (!fileList.isEmpty()) {
+            File sub = fileList.pop();
+
+            File srcMainJava = sourceOf(sub);
+            if (srcMainJava.exists() && srcMainJava.isDirectory()) {
+                typeSolver.add(new JavaParserTypeSolver(srcMainJava));
+                continue;
+            }
+
+            var subFiles = sub.listFiles();
+            if (subFiles != null) {
+                fileList.addAll(List.of(subFiles));
+            }
+        }
+    }
+
+    public static ResolvedReferenceTypeDeclaration resolve(Node node) {
+        return resolver.resolveDeclaration(node, ResolvedReferenceTypeDeclaration.class);
+    }
+
+    public static File sourceOf(File file) {
+        return new File(file, "src/main/java");
+    }
+}
--- src/main/java/translate/complexity/clazz/DITEvaluator.java
+++ src/main/java/translate/complexity/clazz/DITEvaluator.java
@@ -1,8 +1,16 @@
 package translate.complexity.clazz;
 
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.body.RecordDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import com.github.javaparser.resolution.types.ResolvedReferenceType;
 import translate.complexity.ComplexityEvaluator;
 import translate.complexity.ComplexityMetricResult;
+import translate.complexity.ComplexityUtils;
+
+import java.util.Objects;
 
 public class DITEvaluator implements ComplexityEvaluator.Clazz {
     private static final ComplexityMetricResult.ComplexityMetricResultBuilder builder = ComplexityMetricResult.builder()
@@ -12,6 +20,35 @@ public class DITEvaluator implements ComplexityEvaluator.Clazz {
 
     @Override
     public ComplexityMetricResult calculate(Node node) {
-        return null;
+        if (node instanceof RecordDeclaration || node instanceof EnumDeclaration) {
+            return builder.value(1).build();
+        }
+
+        try {
+            ClassOrInterfaceDeclaration decl = (ClassOrInterfaceDeclaration) node;
+            ResolvedReferenceTypeDeclaration type = ComplexityUtils.resolve(decl);
+
+            int depth = 0;
+            while (type != null) {
+                String qName = type.getQualifiedName();
+                if (qName.equals("java.lang.Object")) break;
+                if (type.getQualifiedName().isEmpty()) break;
+                depth++;
+
+                type = type.getAllAncestors()
+                        .stream()
+                        .map(ResolvedReferenceType::getTypeDeclaration)
+                        .map((optional) -> optional.orElse(null))
+                        .filter(Objects::nonNull)
+                        .filter((typeDecl) -> !typeDecl.isInterface())
+                        .findFirst()
+                        .orElse(null);
+            }
+
+            return builder.value(depth).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return builder.value(1).build();
+        }
     }
 }
