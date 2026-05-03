f50477120b4b66c008ae433471ab319320bf9347
Intybyte <vaan.testwork@gmail.com>
2026-05-03 19:53:15 +0200

    Add System level ComplexityEvaluator

--- src/main/java/translate/complexity/ComplexityEvaluator.java
+++ src/main/java/translate/complexity/ComplexityEvaluator.java
@@ -2,10 +2,15 @@ package translate.complexity;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import translate.translator.ComplexityTranslator;
 
 import java.util.Collection;
 
 public interface ComplexityEvaluator {
+    interface System {
+        ComplexityMetricResult calculate(ComplexityTranslator translator, Collection<Node> allClazz);
+    }
+
     interface Clazz {
         ComplexityMetricResult calculate(Collection<Node> allClazz, Node clazz);
     }
